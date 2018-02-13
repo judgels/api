9154bea72674ebb0e5e28dc5f42c95dea9d24a37
Ashar Fuadi <fushar@gmail.com>
2018-02-13 21:12:15 +0700

    Use sealtiel-api

--- src/main/java/org/iatoki/judgels/api/sealtiel/impls/SealtielClientAPIImpl.java
+++ src/main/java/org/iatoki/judgels/api/sealtiel/impls/SealtielClientAPIImpl.java
@@ -1,52 +1,68 @@
 package org.iatoki.judgels.api.sealtiel.impls;
 
-import com.google.gson.JsonObject;
+import com.palantir.remoting.api.errors.RemoteException;
+import com.palantir.remoting3.clients.UserAgent;
+import judgels.sealtiel.api.message.MessageData;
+import judgels.sealtiel.api.message.MessageService;
+import judgels.service.api.client.BasicAuthHeader;
+import judgels.service.api.client.Client;
+import org.iatoki.judgels.api.JudgelsAPIClientException;
 import org.iatoki.judgels.api.impls.AbstractJudgelsClientAPIImpl;
 import org.iatoki.judgels.api.sealtiel.SealtielClientAPI;
 import org.iatoki.judgels.api.sealtiel.SealtielMessage;
 
 public final class SealtielClientAPIImpl extends AbstractJudgelsClientAPIImpl implements SealtielClientAPI {
+    private final BasicAuthHeader authHeader;
+    private final MessageService messageService;
 
     public SealtielClientAPIImpl(String baseUrl, String clientJid, String clientSecret) {
         super(baseUrl, clientJid, clientSecret);
+
+        UserAgent userAgent = UserAgent.of(UserAgent.Agent.of("judgels", UserAgent.Agent.DEFAULT_VERSION));
+        messageService = JaxRsClients.create(MessageService.class, baseUrl, userAgent);
+        authHeader = BasicAuthHeader.of(Client.of(clientJid, clientSecret));
     }
 
     @Override
     public SealtielMessage fetchMessage() {
-        return sendPostRequest("/messages/fetch").asObjectFromJson(SealtielMessage.class);
+        try {
+            return messageService.receiveMessage(authHeader)
+                    .map(m -> new SealtielMessage(m.getId(), m.getSourceJid(), "", m.getType(), m.getContent(), ""))
+                    .orElse(null);
+        } catch (RemoteException e) {
+            throw new JudgelsAPIClientException(null, e);
+        }
     }
 
     @Override
     public void acknowledgeMessage(long messageId) {
-        sendPostRequest(interpolatePath("/messages/:messageId/acknowledge", messageId));
+        try {
+            messageService.confirmMessage(authHeader, messageId);
+        } catch (RemoteException e) {
+            throw new JudgelsAPIClientException(null, e);
+        }
     }
 
     @Override
-    public void extendMessageTimeout(long messageId) {
-        sendPostRequest(interpolatePath("/messages/:messageId/extendTimeout", messageId));
-    }
+    public void extendMessageTimeout(long messageId) {}
 
     @Override
     public void sendMessage(String targetClientJid, String messageType, String message) {
-        JsonObject body = new JsonObject();
+        MessageData data = new MessageData.Builder()
+                .targetJid(targetClientJid)
+                .type(messageType)
+                .content(message)
+                .build();
 
-        body.addProperty("targetClientJid", targetClientJid);
-        body.addProperty("messageType", messageType);
-        body.addProperty("message", message);
-        body.addProperty("priority", 3);
-
-        sendPostRequest("/messages/send", body);
+        try {
+            messageService.sendMessage(authHeader, data);
+        } catch (RemoteException e) {
+            throw new JudgelsAPIClientException(null, e);
+        }
     }
 
     @Override
     public void sendLowPriorityMessage(String targetClientJid, String messageType, String message) {
-        JsonObject body = new JsonObject();
-
-        body.addProperty("targetClientJid", targetClientJid);
-        body.addProperty("messageType", messageType);
-        body.addProperty("message", message);
-        body.addProperty("priority", 1);
-
-        sendPostRequest("/messages/send", body);
+        sendMessage(targetClientJid, messageType, message);
     }
 }
